ef34004afa0d68c894bd0a8837991b55ec3e132a
komgrit.yota57@sskru.ac.th <komgrit.yota57@sskru.ac.th>
2016-09-09 14:29:22 +0700

    add project to repository

---
+++ open source/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_source
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Komgrit Yota 5711423111");
+
+            //Keep the consloe window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
